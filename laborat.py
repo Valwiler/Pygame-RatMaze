0b0f0649a76ecca3c1110587f319ae0e388f60e3
Clément Salino <clemclemclem@protonmail.com>
2020-10-02 16:55:31 -0400

    Ajout d'une classe Acteur et création du fromage

--- laborat.py
+++ laborat.py
@@ -1,13 +1,17 @@
 from sys import exit
 import pygame
 
-LFEN = 640
-HFEN = 480
-LRECT = 40
-HRECT = 20
-POSX = 100
-POSY = 50
+LFEN = 1024
+HFEN = 640
+LRECT = 64
+HRECT = 64
+POSX_PLAYER = 12
+POSY_PLAYER = 12
+POSX_CHEESE = 800
+POSY_CHEESE = 400
+ZOMBIES = [(200,160),(400,360),(600,560)]
 
+J = (255,255,0)
 R = (255,0,0)
 G = (0,255,0)
 B = (0,0,255)
@@ -16,12 +20,19 @@ N = (0,0,0)
 MVT = 10
 FR = 60
 
-class Jeu():
-    def __init__(self, lf, hf, lr, hr, posx, posy, couleur):
-        self.lf, self.hf = lf, hf
-        self.lr, self.hr = lr, hr
+class Acteur():
+    def __init__(self, posx, posy, couleur):
         self.posx, self.posy = posx, posy
         self.couleur = couleur
+        self.alive = True
+
+class Jeu():
+    def __init__(self, lf, hf, lr, hr, couleur):
+        self.lf, self.hf = lf, hf
+        self.lr, self.hr = lr, hr
+        
+        self.joueur = Acteur(POSX_PLAYER, POSY_PLAYER, B)
+        self.fromage = Acteur(POSX_CHEESE, POSY_CHEESE, J)
         
         self.fenetre = pygame.display.set_mode((self.lf, self.hf))
         
@@ -41,35 +52,31 @@ class Jeu():
                 self.en_marche = False
                 break
             elif evenement.type == pygame.KEYDOWN:
-                if evenement.key == pygame.K_r:
-                    self.couleur = R
-                elif evenement.key == pygame.K_g:
-                    self.couleur = G
-                elif evenement.key == pygame.K_b:
-                    self.couleur = B
-                elif evenement.key == pygame.K_RIGHT:
-                    self.posx += MVT
+                if evenement.key == pygame.K_RIGHT:
+                    self.joueur.posx += MVT
                 elif evenement.key == pygame.K_LEFT:
-                    self.posx -= MVT
+                    self.joueur.posx -= MVT
                 elif evenement.key == pygame.K_DOWN:
-                    self.posy += MVT
+                    self.joueur.posy += MVT
                 elif evenement.key == pygame.K_UP:
-                    self.posy -= MVT
+                    self.joueur.posy -= MVT
 
     def actualiser(self):
         pass
         
     def rendre(self):
         self.fenetre.fill(N)
-        pygame.draw.rect(self.fenetre,
-                        self.couleur,
-                        (self.posx, self.posy, self.lr, self.hr))
+        #JOUEUR
+        pygame.draw.rect(self.fenetre,self.joueur.couleur,(self.joueur.posx, self.joueur.posy, self.lr, self.hr))
+        #FROMAGE
+        pygame.draw.rect(self.fenetre,self.fromage.couleur,(self.fromage.posx, self.fromage.posy, self.lr, self.hr))
+
         pygame.display.update()
 
 
 def main():
     pygame.init()
-    jeu = Jeu(LFEN, HFEN, LRECT, HRECT, POSX, POSY, R)
+    jeu = Jeu(LFEN, HFEN, LRECT, HRECT, B)
     jeu.executer()
     pygame.quit()
     
