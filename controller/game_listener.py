649f5dbf1198e181a3a79521ba0f59e4ea2fc25e
Clément Salino <clemclemclem@protonmail.com>
2020-10-24 16:38:47 -0400

    vrai ajout du game listener

--- controller/game_listener.py
+++ controller/game_listener.py
@@ -1,21 +1,21 @@
 import pygame
 
 class GameListener():
-
+    
     def __init__(self, jeu):
-        self.jeu = jeu
+        self.game = game
 
     def get_input(self):
         while self.game.en_marche:
             keys = pygame.key.get_pressed()
             if keys[pygame.K_ESCAPE]:
-                self.jeu.en_marche = False
+                self.game.en_marche = False
                 break
             if keys[pygame.K_UP]:
-                self.jeu.joueur.posy -= self.game.MVT
+                self.game.player.posy -= self.game.MVT
             if keys[pygame.K_DOWN]:
-                self.jeu.joueur.posy += self.game.MVT
+                self.game.player.posy += self.game.MVT
             if keys[pygame.K_LEFT]:
-                self.jeu.joueur.posx -= self.game.MVT
+                self.game.player.posx -= self.game.MVT
             if keys[pygame.K_RIGHT]:
-                self.jeu.joueur.posx += self.game.MVT
+                self.game.player.posx += self.game.MVT
