1f28c3f2a21f2f8c2d7b32c31bd88868071e3731
Clément Salino <clemclemclem@protonmail.com>
2020-10-25 10:47:20 -0400

    ajout du changement de couleur

--- view/Window.py
+++ view/Window.py
@@ -6,23 +6,27 @@ import model.actor_factory as acf
 
 class Window:
 
+
     def __init__(self, width, height):
         self.ecran = pg.display.set_mode((width, height))
+        self.background_color = (0, 0, 0)
 
 
     def update_icons(self, act_factory):
        # if acf.act_factory.get_actor(0).get_alive is False:
             #self.ecran.fill((255,255,255))
            # pg.display.
-        self.ecran.fill((0, 0, 0))
+        self.ecran.fill(self.background_color)
         for icons in act_factory:
             if  isinstance(icons, a.Player):
                 for icone2 in act_factory:
                     if i.IconesActeurs(icons, 64).isinCollision(i.IconesActeurs(icone2, 64)):
                         if isinstance(icone2, a.Zombie):
+                            self.background_color = (255,0,0)
                             icons.set_alive(False)
                             print('game over')
                         elif isinstance(icone2,a.Fromage):
+                            self.background_color = (255,255,255)
                             print('Yay, you won !!!')
                         else:
                             pass
